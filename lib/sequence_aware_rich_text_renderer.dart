875f9891dcd2219780964bbbbcbf380b34e39745
The Full-Stack Jonin <francis94c@gmail.com>
2024-09-22 15:17:17 +0100

    Initial Commit.

---
+++ lib/sequence_aware_rich_text_renderer.dart
@@ -0,0 +1,7 @@
+library sequence_aware_rich_text_renderer;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
---
+++ test/character_aware_rich_text_field_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:sequence_aware_rich_text_renderer/sequence_aware_rich_text_renderer.dart';
+
+void main() {
+  test('adds one to input values', () {});
+}
