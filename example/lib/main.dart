99373e76f9e55753b26295afb6999be21c765ed0
The Full-Stack Jonin <francis94c@gmail.com>
2024-09-23 00:33:46 +0100

    refactor: Remove unused code.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -47,7 +47,6 @@ class _MyHomePageState extends State<MyHomePage> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Text(""),
                 RichText(
                   text: const TextSpan(
                     text:
